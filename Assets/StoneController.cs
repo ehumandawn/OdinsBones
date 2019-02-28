ac91c7da6534d61f36b069d62940f53cbd7be0f8
Nicole Sallak Anderson <nicole.sallak.anderson@gmail.com>
2019-02-27 17:04:46 -0800

    trying to throw 24 stones
    and they're all bouncing off the cube

---
+++ Assets/StoneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneController : MonoBehaviour {
+	
+	[SerializeField] private GameObject StonePrefab;
+	private GameObject[] _stone = new GameObject[24];
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < _stone.Length; i++) {
+			_stone [i] = null;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		for (int i = 0; i < _stone.Length; i++) {
+			if (_stone[i] == null) {
+				_stone[i] = Instantiate (StonePrefab) as GameObject;
+				_stone[i].transform.position = new Vector3 (5, 12, -(25-i));
+			}
+		
+		}
+
+	}
+
+}
