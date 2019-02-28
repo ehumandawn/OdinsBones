42eff1f902dca64d9aff810cc82f6b33f9e8bd4e
Nicole Sallak Anderson <nicole.sallak.anderson@gmail.com>
2019-02-27 19:48:23 -0800

    improved stone instantiation

--- Assets/StoneController.cs
+++ Assets/StoneController.cs
@@ -17,12 +17,16 @@ public class StoneController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
+		// this needs to have code to instantiate 24 different rocks with runes on each one
+		// AND it needs to know which stones have stopped moving, those are the ones that are read
+		// AND those stones must fall face up in the box
+		// AND be random, not the same stones should land in the box each time
+		// AND somehow the text that goes with the stones must be printed. An ArrayList of 24 different stones perhaps?
 
 		for (int i = 0; i < _stone.Length; i++) {
 			if (_stone[i] == null) {
 				_stone[i] = Instantiate (StonePrefab) as GameObject;
-				_stone[i].transform.position = new Vector3 (5, 12, -(25-i));
+				_stone[i].transform.position = new Vector3 (i, 4, -i);
 			}
 		
 		}
