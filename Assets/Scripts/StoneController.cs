b06ab10b82f8754abdb508a521179e0d802da8e1
Nicole Sallak Anderson <nicole.sallak.anderson@gmail.com>
2019-03-13 11:20:49 -0700

    Prefabs work!
    And so does most of the physics. Just need to adjust some parameters to make them fall one one side or the other

--- Assets/Scripts/StoneController.cs
+++ Assets/Scripts/StoneController.cs
@@ -71,73 +71,73 @@ public class StoneController : MonoBehaviour {
 						_stone [count].transform.position = new Vector3 (runeThrown, 4, -2);
 					} else if (runeThrown == 2) {
 						_stone [count] = Instantiate (StonePrefab3) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (runeThrown, 4, -1);
 					} else if (runeThrown == 3) {
 						_stone [count] = Instantiate (StonePrefab4) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (runeThrown, 4, -2);
 					} else if (runeThrown == 4) {
 						_stone [count] = Instantiate (StonePrefab5) as GameObject;
 						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
 					} else if (runeThrown == 5) {
 						_stone [count] = Instantiate (StonePrefab6) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (runeThrown, 4, -4);
 					} else if (runeThrown == 6) {
 						_stone [count] = Instantiate (StonePrefab7) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (runeThrown, 4, -5);
 					} else if (runeThrown == 7) {
 						_stone [count] = Instantiate (StonePrefab8) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (runeThrown, 4, -6);
 					} else if (runeThrown == 8) {
 						_stone [count] = Instantiate (StonePrefab9) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (runeThrown, 4, -7);
 					} else if (runeThrown == 9) {
 						_stone [count] = Instantiate (StonePrefab10) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (runeThrown, 4, -8);
 					} else if (runeThrown == 10) {
 						_stone [count] = Instantiate (StonePrefab11) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (runeThrown, 4, -9);
 					} else if (runeThrown == 11) {
 						_stone [count] = Instantiate (StonePrefab12) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (1, 4, -runeThrown);
 					} else if (runeThrown == 12) {
 						_stone [count] = Instantiate (StonePrefab13) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (2, 4, -runeThrown);
 					} else if (runeThrown == 13) {
 						_stone [count] = Instantiate (StonePrefab14) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (3, 4, -runeThrown);
 					} else if (runeThrown == 14) {
 						_stone [count] = Instantiate (StonePrefab15) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (4, 4, -runeThrown);
 					} else if (runeThrown == 15) {
 						_stone [count] = Instantiate (StonePrefab16) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (5, 4, -runeThrown);
 					} else if (runeThrown == 16) {
 						_stone [count] = Instantiate (StonePrefab17) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (6, 4, -runeThrown);
 					} else if (runeThrown == 17) {
 						_stone [count] = Instantiate (StonePrefab18) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (7, 4, -runeThrown);
 					} else if (runeThrown == 18) {
 						_stone [count] = Instantiate (StonePrefab19) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (8, 4, -runeThrown);
 					} else if (runeThrown == 19) {
 						_stone [count] = Instantiate (StonePrefab20) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (9, 4, -runeThrown);
 					} else if (runeThrown == 20) {
 						_stone [count] = Instantiate (StonePrefab21) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (10, 4, -runeThrown);
 					} else if (runeThrown == 21) {
 						_stone [count] = Instantiate (StonePrefab22) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (runeThrown, 4, -runeThrown);
 					} else if (runeThrown == 22) {
 						_stone [count] = Instantiate (StonePrefab23) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (runeThrown, 4, -runeThrown);
 					} else if (runeThrown == 23) {
 						_stone [count] = Instantiate (StonePrefab24) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (runeThrown, 4, -runeThrown);
 					} else if (runeThrown == 24) {
 						_stone [count] = Instantiate (StonePrefab25) as GameObject;
-						_stone [count].transform.position = new Vector3 (runeThrown, 4, -3);
+						_stone [count].transform.position = new Vector3 (runeThrown, 4, -runeThrown);
 					}
 					count++;  //this should only happen if a new rune is picked
 				}
