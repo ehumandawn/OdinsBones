8f71fbbd47a84ee78170eba166851de6d83e7c27
Nicole Sallak Anderson <nicole.sallak.anderson@gmail.com>
2019-03-22 18:30:18 -0700

    Added opening scene and sound
    Now has the opening/instructions scene and sound, both playing in background and sounds when runes hit the table.
    
    NOTE: Will have to replace slider as triggering scene change with gyroscope when on the phone.

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+//using UnityEngine.EventSystems;
+
+
+
+
+public class LevelController : MonoBehaviour {
+	// this function is used to switch from opening instructions to throwing the runes
+	// when deployed to Android, the gyroscope will be the trigger that switches scenes.
+	// right now, I'm forcing the switch with a slider.
+
+	Slider slider;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+
+	public void Awake () {
+		slider = GetComponent<Slider> ();
+		//if (slider.value == 1) {
+			slider.onValueChanged.AddListener (delegate {
+				OnSliderWasChanged ();
+			});
+			OnSliderWasChanged ();
+		//}
+	}
+
+	public void OnSliderWasChanged() {
+		if (slider.value == 1) {
+		//Debug.Log ("Not sure why we got here since I haven't touched the sider.");
+		SceneManager.LoadScene (1);
+		}
+	}
+}
---
+++ Assets/Scripts/SoundScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour {
+
+	public AudioSource StoneSound;
+
+	// Use this for initialization
+	void Start () {
+		StoneSound = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		StoneSound.Play ();
+	}
+}
