c89d2e6cb9f1ac91caceab25e9ba6ef7e48b0cdd
Nicole Sallak Anderson <nicole.sallak.anderson@gmail.com>
2019-04-09 17:42:35 -0700

    Added accelerometer and touch screen stuff
    Almost done. So exciting

---
+++ Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Accelerometer : MonoBehaviour {
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 shake = Input.acceleration;
+
+		if (shake.sqrMagnitude > 3.0) {
+			SceneManager.LoadScene (1);
+		}
+	}
+}
