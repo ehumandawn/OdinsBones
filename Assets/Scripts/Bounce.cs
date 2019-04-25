ebe715433d75cbfd6cbcaeca158be35a4f2a70fd
Nicole Sallak Anderson <nicole.sallak.anderson@gmail.com>
2019-04-24 19:46:35 -0700

    More changes to make the runes tumble better

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+
+	private float minVelocity = 25.0f;
+	private Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.name != "Table") {
+			body = GetComponent<Rigidbody> ();
+			//body.transform.Rotate (90, 0, 90);
+			Deflection (collision.contacts [0].normal);
+		}
+	}
+
+	private void Deflection (Vector3 collisionNormal) {
+		var speed = body.velocity.magnitude;
+		var direction = Vector3.Reflect(body.velocity.normalized, collisionNormal);
+		body.velocity = direction * Mathf.Max(speed, minVelocity);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
